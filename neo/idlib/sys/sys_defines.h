89674217ab9199e23a2e842c4d3df65b503e0815
Karin <beyondk2000@163.com>
2024-05-30 20:27:52 +0800

    Linux arm 32bits compile # Conflicts: #	neo/idlib/sys/sys_defines.h

--- neo/idlib/sys/sys_defines.h
+++ neo/idlib/sys/sys_defines.h
@@ -119,6 +119,8 @@ If you have questions concerning this license or the applicable additional terms
 #define CPUSTRING 						"aarch64"
 #elif defined(__powerpc64__)
 #define CPUSTRING						"ppc64"
+#elif defined(__arm__)
+#define CPUSTRING						"arm"
 #else
 #error unknown CPU
 #endif

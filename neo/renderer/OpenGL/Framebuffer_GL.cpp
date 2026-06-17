c8a11564a0fa7205ef8806f1a16075d644d11a04
sandstranger <sandstranger@gmail.com>
2026-06-17 19:27:23 +0400

    downscale geometryBufferFBO format

--- neo/renderer/OpenGL/Framebuffer_GL.cpp
+++ neo/renderer/OpenGL/Framebuffer_GL.cpp
@@ -487,7 +487,7 @@ void Framebuffer::Init()
     globalFramebuffers.geometryBufferFBO->Bind();
 #endif
 
-    globalFramebuffers.geometryBufferFBO->AddColorBuffer(GL_RGBA16F, 0);
+    globalFramebuffers.geometryBufferFBO->AddColorBuffer(GL_RGB10_A2, 0);
     globalFramebuffers.geometryBufferFBO->AddStencilBuffer(GL_DEPTH24_STENCIL8);
     globalFramebuffers.geometryBufferFBO->AttachImage2D(GL_TEXTURE_2D, globalImages->currentNormalsImage, 0);
     globalFramebuffers.geometryBufferFBO->AttachImageDepth(GL_TEXTURE_2D, globalImages->currentDepthImage);
